6be98cd8b2941061a34d2906af2a611848d2d154
Shivam181103 <you@example.com>
2022-02-24 12:00:33 +0530

    ..

--- social-publi/src/App.js
+++ social-publi/src/App.js
@@ -4,9 +4,9 @@ import './components/Influencer/influencer.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Services from './components/Services/services.jsx';
-import Brands from './components/Brands/brands.jsx';
+import Brands from './components/Brands/Brands.jsx';
 import Tools from './components/Tools/tools.jsx';
-import Home from './components/Home/home.jsx';
+import Home from './components/Home/Home.jsx';
 import TestimonialProfile from './components/Testimonial-profile';
 import ContactUs from './components/contactUs';
 import { BrowserRouter as Router,Routes ,Switch ,Route, BrowserRouter } from 'react-router-dom';
@@ -16,18 +16,7 @@ function App() {
     <>
 
     <Router>
- 
-         <Navbar/>
-             <Routes>
-             <Route path='/' exact element={<Home/>}/>
-             <Route path='/influencer' element={<Influencer/>}/>
-             <Route path='/contactus' element={<ContactUs/>}/>
-             <Route path='/services' element={<Services/>}/>
-             <Route path='/brands' element={<Brands/>}/>
-             <Route path='/tools' element={<Tools/>}/>
-             <Route path='/influencer/testimonial' element={<TestimonialProfile
-               />}/>
-             </Routes>
+  
              
  
         <Navbar/>
